5bfab70968377398de8ff5e8dd229077157ce0ce
AdriAlmohalla <almohallabautista@gmail.com>
2023-02-16 19:38:05 +0100

    pagina tarifas y contacto terminada

--- src/app/page-contacto/page-contacto.component.ts
+++ src/app/page-contacto/page-contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faPhone, faEnvelope, faMapLocation } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-page-contacto',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-contacto.component.scss']
 })
 export class PageContactoComponent {
+
+  faPhone = faPhone
+  faEnvelope = faEnvelope
+  faMapLocation = faMapLocation
 
 }
