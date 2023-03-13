83b224c7a5adb28662d425051e8bb7ce9dddf355
AdriAlmohalla <almohallabautista@gmail.com>
2023-03-13 19:32:53 +0100

    favicon

--- src/app/page-psicologia/page-psicologia.component.ts
+++ src/app/page-psicologia/page-psicologia.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faHandshake } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-page-psicologia',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-psicologia.component.scss']
 })
 export class PagePsicologiaComponent {
+
+  faHandshake = faHandshake;
 
 }
