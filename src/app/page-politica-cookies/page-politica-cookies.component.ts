27ee486d97bb27e248c766e22f7cb69fee162ee3
AdriAlmohalla <almohallabautista@gmail.com>
2023-05-09 18:41:31 +0200

    cookies terminado

--- src/app/page-politica-cookies/page-politica-cookies.component.ts
+++ src/app/page-politica-cookies/page-politica-cookies.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+declare let gtag: Function;
+declare let loadGoogleAnalytics: Function;
+
 @Component({
   selector: 'app-page-politica-cookies',
   templateUrl: './page-politica-cookies.component.html',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class PagePoliticaCookiesComponent {
 
+  constructor() { }
+
+  acceptCookies(event: Event) {
+    event.preventDefault();
+    localStorage.setItem('cookies_accepted', 'true');
+    loadGoogleAnalytics();
+  }
+
+  rejectCookies(event: Event) {
+    event.preventDefault();
+    localStorage.setItem('cookies_accepted', 'false');
+    this.deleteAllCookies();
+  }
+
+  deleteAllCookies() {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf('=');
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
+    }
+  }
 }
